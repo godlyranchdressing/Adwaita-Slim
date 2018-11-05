f34ca4b93522e850bc417aff51e0e3086e2f6c53
Павел <polter.rnd@gmail.com>
2018-11-06 00:31:19 +0300

    Updated README and parse-sass.sh script from mainstream

--- src/gtk-3.0/parse-sass.sh
+++ src/gtk-3.0/parse-sass.sh
@@ -1,4 +1,13 @@
-#!/usr/bin/bash
+#! /bin/bash
+
+if [ ! "$(which sassc 2> /dev/null)" ]; then
+   echo sassc needs to be installed to generate the css.
+   exit 1
+fi
+
+SASSC_OPT="-M -t compact"
+
+echo Generating the css...
 
 #bundle exec sass --update --sourcemap=none .
 sass --update --sourcemap=none .
